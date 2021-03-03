3f2cc33d3f35f7e19d25ba2bba661ea530864eb6
CVPR2021Submit <74448065+CVPR2021Submit@users.noreply.github.com>
2021-03-03 09:14:57 +0800

    Update audioclass2.py

--- audio/audioclass2.py
+++ audio/audioclass2.py
@@ -9,6 +9,7 @@ import numpy as np
 from Soundmodel import SoundNet
 import argparse
 
+classes = 28
 class att_Model(nn.Module):
     def __init__(self):  # 128,128,512,29
         super(att_Model, self).__init__()
@@ -16,22 +17,17 @@ class att_Model(nn.Module):
         checkpoint = torch.load('vggsound_netvlad.pth.tar')
         Amodel.load_state_dict(checkpoint['model_state_dict'])
         Amodel = list(Amodel.audnet.children())
-        self.layerA_0 = nn.Sequential(*Amodel[:4])
-        self.layerA_1 = Amodel[4]
-        self.layerA_2 = Amodel[5]
-        self.layerA_3 = Amodel[6]
-        self.layerA_4 = Amodel[7]
-        self.layerA_p1 = Amodel[8]
-        self.Afc1 = nn.Linear(8192, 2)
+        self.layerA = nn.Sequential(*Amodel[:8])
+        self.Afc1 = nn.Linear(9216, 4096)
+        self.Afc2 = nn.Linear(4096, 1024)
+        self.Afc3 = nn.Linear(1024, 2)
+        self.Afc = nn.Linear(9216, 4096)
 
     def forward(self, audio):
-        layerA_0 = self.layerA_0(audio)                         # 1,1,257,48->1,64,65,12
-        layerA_1 = self.layerA_1(layerA_0)                      # 1,64,65,12->1,64,65,12
-        layerA_2 = self.layerA_2(layerA_1)                      # 1,128,33,6
-        layerA_3 = self.layerA_3(layerA_2)                      # 1,256,17,3
-        layerA_4 = self.layerA_4(layerA_3)                      # 1,512,9,2
-        layerA_p1 = self.layerA_p1(layerA_4)                    # 1,8192
-        layerA_p1 = layerA_p1.reshape(layerA_p1.size(0), -1)
-        Afc1 = self.Afc1(layerA_p1)# 1,28
-
-        return Afc1
+        layerA = self.layerA(audio)                         # 1,1,257,48->1,64,65,12
+        layerA_p = layerA.reshape(layerA.size(0), -1)
+        Afc1 = F.relu(self.Afc1(layerA_p)) # 1,28
+        Afc2 = F.relu(self.Afc2(Afc1))
+        Afc3 = F.relu(self.Afc3(Afc2))
+        # Afc = F.relu(self.Afc(layerA_p))
+        return torch.softmax(Afc3, 1)
