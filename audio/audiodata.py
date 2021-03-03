e40db953da18a4a73cb187892d4230efef5d2ef5
CVPR2021Submit <74448065+CVPR2021Submit@users.noreply.github.com>
2021-03-03 09:25:15 +0800

    Update audiodata.py

--- audio/audiodata.py
+++ audio/audiodata.py
@@ -6,14 +6,14 @@ from PIL import Image, ImageStat
 import os
 import h5py
 from PIL import Image
+import random
 from torchvision import transforms
 
-Audio_path = "F:\\wgt\\AVE\\AVE_Dataset\\audio2\\"
-Crop_path = 'E:\\crop\\crop\\'
-Forg_path = 'F:\\wgt\\AVE\\AVE_Dataset\\forgori\\'
+Audio_path = ".\AVE\AVE_Dataset\audio_features\"
+Youyin_path = '.\AVE\AVE_Dataset\audiotrue\'  # audio  feature is true
 def make_dataset(ori_path):
-    path_list = []
-    sequ = -1
+    path_listz = []
+    path_listo = []
     count = 0
     ori_name = os.listdir(ori_path)
     for file in range(0, len(ori_name)):
@@ -23,15 +23,19 @@ def make_dataset(ori_path):
         for fs in range(0, len(ficname)):
             picpath = os.path.join(ficpath, ficname[fs])
             picname = os.listdir(picpath)
-            sequ = sequ + 1
             for picp in range(0, len(picname)):
-                onoroff = '0'
-                if os.path.exists(os.path.join(Forg_path, ori_name[file], ficname[fs], picname[picp][:-4]+'.jpg')):
+                if os.path.exists(os.path.join(Youyin_path, ori_name[file], ficname[fs], picname[picp][:-4]+'.jpg')):
                     onoroff = '1'
                     pa = os.path.join(Audio_path, ori_name[file], ficname[fs], picname[picp][:-4]+'_asp.h5')
-                    path_list.append(onoroff+'+'+pa+'+'+str(file)+'+'+
-                                    ori_name[file]+'+'+ficname[fs]+'+'+picname[picp][:-4]+'.jpg')
-    return path_list
+                    path_listo.append(onoroff+'+'+pa+'+'+str(file)+'+'+ori_name[file]+'+'+ficname[fs]+'+'+picname[picp][:-4]+'.jpg')
+                else:
+                    onoroff = '0'
+                    pa = os.path.join(Audio_path, ori_name[file], ficname[fs], picname[picp][:-4]+'_asp.h5')
+                    path_listz.append(onoroff+'+'+pa+'+'+str(file)+'+'+ ori_name[file]+'+'+ficname[fs]+'+'+picname[picp][:-4]+'.jpg')
+    random.shuffle(path_listz)
+    slice = random.sample(path_listz, len(path_listo))
+    path_listo = path_listo+ slice
+    return path_listo
 
 
 class ImageFolder(Dataset):
@@ -48,12 +52,11 @@ class ImageFolder(Dataset):
             audio_features = np.float32(hf['dataset'][:])  # 5,128
         audio_features_batch = torch.from_numpy(audio_features).float()
         inda = int(img_la[-4])
-        indv = int(img_la[-5])
         file = img_la[-3]
         subfile = img_la[-2]
         ssubfile = img_la[-1]
 
-        return audio_features_batch, onoroff, indv, inda, file, subfile, ssubfile
+        return audio_features_batch, onoroff, inda, file, subfile, ssubfile
 
     def __len__(self):
         return len(self.imgs)
