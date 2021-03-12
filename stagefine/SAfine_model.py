e9723c83acd15be1f4fe5e3b7942a30c747cf557
CVPR2021Submit <74448065+CVPR2021Submit@users.noreply.github.com>
2021-03-12 14:52:48 +0800

    Update SAfine_model.py

--- stagefine/SAfine_model.py
+++ stagefine/SAfine_model.py
@@ -17,31 +17,32 @@ class att_Model(nn.Module):
     def __init__(self):  # 128,128,512,29
         super(att_Model, self).__init__()
         Amodel = SoundNet()
-        Amodel = torch.load('SingleAModel.pt')
-        Amodel = list(Amodel.children())
-        self.layerA_0 = Amodel[0]
-        self.layerA_1 = Amodel[1]
-        self.layerA_2 = Amodel[2]
-        self.layerA_3 = Amodel[3]
-        self.layerA_4 = Amodel[4]
-        self.layerA_p = Amodel[5]
+        checkpoint = torch.load('vggsound_netvlad.pth.tar')
+        Amodel.load_state_dict(checkpoint['model_state_dict'])
+        Amodel = list(Amodel.audnet.children())
+        self.layerA_0 = nn.Sequential(*Amodel[:4])
+        self.layerA_1 = Amodel[4]
+        self.layerA_2 = Amodel[5]
+        self.layerA_3 = Amodel[6]
+        self.layerA_4 = Amodel[7]
+        self.layerA_p = Amodel[8]
+        self.Aup1 = nn.ConvTranspose2d(
+            8192, 32, kernel_size=4, stride=2, padding=0)
+        self.Aup2 = nn.ConvTranspose2d(
+            32, 32, kernel_size=4, stride=2, padding=0)
+        self.Aup3 = nn.ConvTranspose2d(
+            32, 32, kernel_size=3, stride=1, padding=0)
 
-        Vmodel = att_Net()
-        Vmodel = torch.load('9001.pt')
-        Vmodel = list(Vmodel.children())
-        self.layerV_0 = Vmodel[0]
-        self.layerV_1 = Vmodel[1]
-        self.layerV_2 = Vmodel[2]
-        self.layerV_3 = Vmodel[3]
-        self.layerV_4 = Vmodel[4]
+        net = torch.hub.load('facebookresearch/WSL-Images',
+                             'resnext101_32x8d_wsl')
+        net = list(net.children())
+        self.layerV_0 = nn.Sequential(*net[:4])
+        self.layerV_1 = net[4]
+        self.layerV_2 = net[5]
+        self.layerV_3 = net[6]
+        self.layerV_4 = net[7]
 
         self.relu = nn.ReLU()
-        self.Aup1 = nn.ConvTranspose2d(
-            8192, 64, kernel_size=4, stride=2, padding=0)
-        self.Aup2 = nn.ConvTranspose2d(
-            64, 64, kernel_size=4, stride=2, padding=0)
-        self.Aup3 = nn.ConvTranspose2d(
-            64, 64, kernel_size=3, stride=1, padding=0)
         self.layerV_down = nn.Conv2d(2048, 64, 1)
         self.attention = nn.Conv2d(128, 1, 1)
         self.atten_conv = nn.Conv2d(128, 128, 1)
@@ -50,7 +51,7 @@ class att_Model(nn.Module):
         self.Vatten_pool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
         self.Vatten_fc = nn.Linear(2048, classes)
 
-    def forward(self, switch, audio, video):
+    def forward(self, audio, video， switch):
         layerV_0 = self.layerV_0(video)
         layerV_1 = self.layerV_1(layerV_0)
         layerV_2 = self.layerV_2(layerV_1)
@@ -71,7 +72,7 @@ class att_Model(nn.Module):
         fuse_conv = self.atten_conv(torch.cat((Aup3, self.layerV_down(layerV_4)), dim=1))
 
         attention = self.attention(fuse_conv)
-        Vattention = F.relu((switch[0,1]*F.sigmoid(attention) * layerV_4) + layerV_4)
+        Vattention = F.relu((switch.view(video.size(0),1,1,1)*F.sigmoid(attention) * layerV_4) + layerV_4)
         Vattention = self.Vatten_conv(Vattention)
         Vatten_pool = self.Vatten_pool(Vattention)
         Vatten_pool = Vatten_pool.view(Vatten_pool.size(0), -1)
